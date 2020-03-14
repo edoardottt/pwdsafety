1e9d2c1f054e0fdfa8fc4b80fb346fe647ff4149
Edoardo Ottavianelli <ottavianelli.1756005@studenti.uniroma1.it>
2020-03-14 22:32:29 +0100

    test

--- Utils/Read-input.go
+++ Utils/Read-input.go
@@ -6,6 +6,15 @@ import (
 	"os"
 )
 
+//Reading all inputs from stdin
+func ReadInput() []string {
+	var result []string
+	inputs := [5]string{"name","surname","birthday(ddmmyyyy)","telephone number","pet's name"}
+	for _, value := range inputs {
+		result = append(result,ReadSingleInput(value))
+	}
+	return result
+}
 
 //Reading one single input
 func ReadSingleInput(input string) string {
--- main/main.go
+++ main/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	inputs := Utils.ReadInput()
 	words := Utils.ReadWords("words.txt")
 	color.Green("Prints %s in green.", "text")
 	color.Red("We have red")
