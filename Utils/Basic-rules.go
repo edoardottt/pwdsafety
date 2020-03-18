3326d9f69db4da2564e5d533a2e932a1229a536f
Edoardo Ottavianelli <ottavianelli.1756005@studenti.uniroma1.it>
2020-03-18 19:21:53 +0100

    add basic rules file

---
+++ Utils/Basic-rules.go
@@ -0,0 +1,26 @@
+package Utils
+
+
+func HowManyTypes(password string) int {
+
+}
+
+func HowManyDifferents(password string) int {
+
+}
+
+func isThereUpperCase(password string) bool {
+
+}
+
+func isThereLowerCase(password string) bool {
+
+}
+
+func isThereSymbol(password string) bool {
+
+}
+
+func isThereNumber(password string) bool {
+
+}
--- main/main.go
+++ main/main.go
@@ -7,12 +7,10 @@ import (
 )
 
 func main() {
-	//inputs := Utils.ReadInput()
 	password := Utils.ReadSingleInput("Password")
 	words := Utils.ReadWords("words.txt")
 	color.Green("Prints %s in green.", "text")
 	color.Red("We have red")
-	fmt.Printf("%v", words)
 	println(password)
 	boo := Utils.FindExactly(words,password)
 	fmt.Printf("%t", boo)
