66f92bcf2cd6de83e30b32f44b855e08bb12dffc
edoardottt <edoardott@gmail.com>
2020-03-22 11:46:29 +0100

    Scoring / deleted main/

--- Utils/Scoring.go
+++ Utils/Scoring.go
@@ -5,9 +5,9 @@ Scores known password
 	total = -18
 	It's present = -18
 	There isn't = 0
- */
+*/
 func KnownPwdScore(words []string, password string) float64 {
-	found := FindExactly(words,password)
+	found := FindExactly(words, password)
 	if found {
 		return -18
 	}
@@ -21,7 +21,7 @@ Scores known reversed password
 	There isn't = 0
 */
 func KnownPwdReverseScore(words []string, password string) float64 {
-	foundReverse := FindExactlyReversed(words,password)
+	foundReverse := FindExactlyReversed(words, password)
 	if foundReverse {
 		return -8
 	}
@@ -35,20 +35,34 @@ Scores password's length
 	length <= 6 = 4
 	length==7 = 6
 	length==8 = 10
-	8 < length < 12 = 17
-	11 < length < 15 = 24
+	8 < length < 12 = 14
+	11 < length < 15 = 22
 	14 < length < 19 = 27
 	length >=19 = 30
 */
 func LengthScore(password string) float64 {
 	length := len(password)
-	if length <=4 { return 0 }
-	if length <= 6 { return 4 }
-	if length==7 { return 6 }
-	if length==8 { return 10 }
-	if length > 8 && length < 12 { return 17 }
-	if length > 11 && length < 15 { return 24 }
-	if length > 14 && length < 19 { return 27 }
+	if length <= 4 {
+		return 0
+	}
+	if length <= 6 {
+		return 4
+	}
+	if length == 7 {
+		return 6
+	}
+	if length == 8 {
+		return 10
+	}
+	if length > 8 && length < 12 {
+		return 14
+	}
+	if length > 11 && length < 15 {
+		return 22
+	}
+	if length > 14 && length < 19 {
+		return 27
+	}
 	return 30
 }
 
@@ -66,10 +80,18 @@ func CompositionPwdScore(password string) float64 {
 	upper := IsThereUpperCase(password)
 	lower := IsThereLowerCase(password)
 	symbols := IsThereSymbol(password)
-	if numbers { result+=5 }
-	if upper { result+=5 }
-	if lower { result+=5 }
-	if symbols { result+=5 }
+	if numbers {
+		result += 5
+	}
+	if upper {
+		result += 5
+	}
+	if lower {
+		result += 5
+	}
+	if symbols {
+		result += 5
+	}
 	return float64(result)
 }
 
@@ -81,7 +103,7 @@ Scores How many different chars in relation to the length
 func DifferentCharScore(password string) float64 {
 	diffChars := HowManyDifferents(password)
 	total := len(password)
-	return float64((diffChars*15)/total)
+	return float64((diffChars * 15) / total)
 }
 
 /*
@@ -96,23 +118,35 @@ Scores Entropy's password
 */
 func EntropyScore(password string) float64 {
 	entropy := Entropy(password)
-	if entropy <= 28 { return 3 }
-	if entropy > 28  && entropy <=35 { return 8 }
-	if entropy > 35 && entropy <=59 { return 22 }
-	if entropy > 59 && entropy <=80 { return 28 }
-	if entropy > 80 && entropy <=120 { return 32 }
+	if entropy <= 28 {
+		return 3
+	}
+	if entropy > 28 && entropy <= 35 {
+		return 8
+	}
+	if entropy > 35 && entropy <= 59 {
+		return 22
+	}
+	if entropy > 59 && entropy <= 80 {
+		return 28
+	}
+	if entropy > 80 && entropy <= 120 {
+		return 32
+	}
 	return 35
 }
 
 //Grader
 func Grader(words []string, password string) float64 {
-	knownPwd := KnownPwdScore(words,password)
-	knownPwdReverse := KnownPwdReverseScore(words,password)
+	knownPwd := KnownPwdScore(words, password)
+	knownPwdReverse := KnownPwdReverseScore(words, password)
 	lengthScore := LengthScore(password)
 	compositionPwdScore := CompositionPwdScore(password)
 	differentCharScore := DifferentCharScore(password)
 	entropyScore := EntropyScore(password)
-	score :=  knownPwd + knownPwdReverse + lengthScore + compositionPwdScore + differentCharScore + entropyScore
-	if score > 0 { return score}
+	score := knownPwd + knownPwdReverse + lengthScore + compositionPwdScore + differentCharScore + entropyScore
+	if score > 0 {
+		return score
+	}
 	return 0
 }
