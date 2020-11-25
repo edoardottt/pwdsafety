9b10b4f100b6626e62d32eb8b3380cfbb67f4e0c
edoardottt <edoardott@gmail.com>
2020-11-25 15:59:14 +0100

    update

--- pwds/CheckPwds.go
+++ pwds/CheckPwds.go
@@ -1,6 +1,4 @@
 /*
- *    Copyright (C) 2020 Edoardo Ottavianelli
- *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU Public License as published
  *    by the Free Software Foundation, either version 3 of the License, or
