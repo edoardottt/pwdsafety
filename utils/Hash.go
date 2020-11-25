4da2a7e075c556101eb9cc703b5ec16819b73f5d
edoardottt <edoardott@gmail.com>
2020-11-25 15:58:02 +0100

    update

--- utils/Hash.go
+++ utils/Hash.go
@@ -1,3 +1,21 @@
+/*
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Public License as published
+ *    by the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    Author:
+ *      Edoardo Ottavianelli <edoardott@gmail.com>
+ */
+
 package utils
 
 import (
@@ -6,10 +24,12 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"encoding/hex"
+
 	"golang.org/x/crypto/blake2b"
 	"golang.org/x/crypto/blake2s"
 	"golang.org/x/crypto/md4"
 	"golang.org/x/crypto/ripemd160"
+
 	//"golang.org/x/crypto/sha3"
 	"io"
 )
